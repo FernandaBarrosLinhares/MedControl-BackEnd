285c6a927b65548050e5211ed3c5f2a8ae179aed
G3ovani-Andrade <124536228+G3ovani-Andrade@users.noreply.github.com>
2023-10-03 11:37:07 -0300

    Criação da classe para tratamento de exceções

---
+++ src/main/java/br/senai/labmedicine/exceptions/TratadorDeExcecoes.java
@@ -0,0 +1,41 @@
+package br.senai.labmedicine.exceptions;
+
+import br.senai.labmedicine.dtos.validacoes.ErroValidacaoDTO;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class TratadorDeExcecoes {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> tratarEntityNotFound(EntityNotFoundException e){
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> tratarExcecaoEnum(IllegalArgumentException e){
+        return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErroValidacaoDTO>> tratarParametroInvalido(MethodArgumentNotValidException e){
+        List<FieldError> errors = e.getFieldErrors();
+        List<ErroValidacaoDTO> errosDTO = new ArrayList<>();
+        for(FieldError erro : errors){
+            errosDTO.add(new ErroValidacaoDTO(erro));
+        }
+        return new ResponseEntity<>(errosDTO,HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> tratarConflitoCpf(DataIntegrityViolationException e){
+        return new ResponseEntity<>("Cpf já cadastrado",HttpStatus.CONFLICT);
+    }
+}
