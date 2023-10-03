0df1f1e7c54378c53515bfc11e14bdb8c84add52
Eduardo Cesar Veras dos Santos <eduardocvsantos@gmail.com>
2023-10-02 22:59:09 -0300

    base do projeto

---
+++ src/main/java/br/senai/labmedicine/LabmedicineApplication.java
@@ -0,0 +1,13 @@
+package br.senai.labmedicine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabmedicineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabmedicineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/senai/labmedicine/LabmedicineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senai.labmedicine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabmedicineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
