10f36f0d69ce5666fcc248a9e098943b402f9814
G3ovani-Andrade <124536228+G3ovani-Andrade@users.noreply.github.com>
2023-10-05 15:55:16 -0300

    Mudança no tipo do id

--- src/main/java/br/senai/labmedicine/models/Dieta.java
+++ src/main/java/br/senai/labmedicine/models/Dieta.java
@@ -20,7 +20,7 @@ import java.time.LocalTime;
 public class Dieta {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false,length = 100)
