abcdef023dd027c8f6e5ed05b35abbc6f3374805
Patella <jfpatella@gmail.com>
2023-10-07 20:04:34 -0300

    Repositório Medicamento exclusão lista pacientes.

--- src/main/java/br/senai/labmedicine/repositories/MedicamentoRepository.java
+++ src/main/java/br/senai/labmedicine/repositories/MedicamentoRepository.java
@@ -2,6 +2,10 @@ package br.senai.labmedicine.repositories;
 
 import br.senai.labmedicine.models.Medicamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface MedicamentoRepository extends JpaRepository<Medicamento, Long> {
 }
