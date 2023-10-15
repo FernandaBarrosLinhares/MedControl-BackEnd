f5ef79d5c3c50433720b9978845b1c386fc2dc18
G3ovani-Andrade <124536228+G3ovani-Andrade@users.noreply.github.com>
2023-10-15 17:02:47 -0300

    sql para testes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,10 @@
 
-INSERT INTO  endereco (cep, cidade, estado, logradouro, numero, complemento, bairro, referencia)VALUES('12345-678', 'Sua Cidade', 'Seu Estado', 'Rua Exemplo', 123, 'Complemento','Bairro Exemplo', 'Referência');
-INSERT INTO paciente (nome_completo,genero,cpf,telefone,email,status,data_nascimento,rg,estado_civil,naturalidade,contato_emergencia,alergias,cuidados_especificos,convenio,numero_convenio,validade_convenio,endereco_id) VALUES ('Nome Sobrenome',0,'12345678900','12345678901','email@example.com',true,'2000-01-01','123456789',1,'Naturalidade','11223344556','Alergias específicas','Cuidados específicos','Convênio Médico','12345','2023-12-31',1);
+INSERT INTO  endereco (cep, cidade, estado, logradouro, numero, complemento, bairro, referencia)VALUES('88045300', 'Sua Cidade', 'Seu Estado', 'Rua Exemplo', 123, 'Complemento','Bairro Exemplo', 'Referência');
+INSERT INTO  endereco (cep, cidade, estado, logradouro, numero, complemento, bairro, referencia)VALUES('88045397', 'Sua Cidade', 'Seu Estado', 'Rua Exemplo', 123, 'Complemento','Bairro Exemplo', 'Referência');
+INSERT INTO paciente (nome_completo,genero,cpf,telefone,email,status,data_nascimento,rg,estado_civil,naturalidade,contato_emergencia,alergias,cuidados_especificos,convenio,numero_convenio,validade_convenio,endereco_id) VALUES ('Nome Sobrenome',0,'12345678900','12345678901','email@example.com',true,'2000-01-01','123456789',1,'Naturalidade','11223344556','Alergias específicas','Cuidados específicos','Convênio Médico','12345','2023/12',1);
+INSERT INTO paciente (nome_completo,genero,cpf,telefone,email,status,data_nascimento,rg,estado_civil,naturalidade,contato_emergencia,alergias,cuidados_especificos,convenio,numero_convenio,validade_convenio,endereco_id) VALUES ('Nome Sobrenome',0,'12345678902','12345678901','email@example.com',false,'2000-01-01','123456789',1,'Naturalidade','11223344556','Alergias específicas','Cuidados específicos','Convênio Médico','12345','2023/01',2);
 INSERT INTO dieta (nome, data, horario, tipo_dieta, descricao, status, paciente_id)VALUES ('Dieta ruim', '2023-10-09', '12:09:10', 3, 'Uma dieta rica em vegetais e frutas.', true, 1);
+
+
+INSERT INTO usuario (nome_completo,genero,cpf,telefone,email,status,senha,tipo_usuario) VALUES ('João da Silva',0,'12345678900','11999999999','joao1.silva@email.com',TRUE,'123456',1);
+INSERT INTO usuario (nome_completo,genero,cpf,telefone,email,status,senha,tipo_usuario) VALUES ('João da Silva',0,'12345678905','11999999999','joao2.silva@email.com',TRUE,'123456',2);
