14a901781b963f4414edbb68fd55a007e904fb4f
G3ovani-Andrade <124536228+G3ovani-Andrade@users.noreply.github.com>
2023-10-15 16:59:54 -0300

    mudança na rota para dietas

--- src/main/java/br/senai/labmedicine/controllers/DietaController.java
+++ src/main/java/br/senai/labmedicine/controllers/DietaController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/dieta")
+@RequestMapping("/dietas")
 public class DietaController {
 
     @Autowired
