6c3428c863b3c03f69abca980771abc614ee560c
G3ovani-Andrade <124536228+G3ovani-Andrade@users.noreply.github.com>
2023-10-15 17:16:01 -0300

    mudança nas buscas por nome

--- src/main/java/br/senai/labmedicine/repositories/DietaRepository.java
+++ src/main/java/br/senai/labmedicine/repositories/DietaRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface DietaRepository extends JpaRepository<Dieta,Long> {
-
-    @Query(value = "SELECT d.id,d.nome,d.data,d.horario,d.tipo_dieta,d.descricao,d.status,d.paciente_id FROM dieta d inner join paciente as p on p.nome_completo = :nomePaciente order by d.data,d.horario",nativeQuery = true)
-    List<Dieta> BuscaPorNomePaciente(String nomePaciente);
+    List<Dieta> findAllByPacienteNomeCompletoOrderByDataAscHorario(String nomeCompleto);
 
 }
--- src/main/java/br/senai/labmedicine/repositories/PacienteRepository.java
+++ src/main/java/br/senai/labmedicine/repositories/PacienteRepository.java
@@ -3,5 +3,9 @@ package br.senai.labmedicine.repositories;
 import br.senai.labmedicine.models.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+
+    List<Paciente> findByNomeCompleto(String nomeCompleto);
 }
