e91262bf11338758422ca2dfc561fe5876745a6b
G3ovani-Andrade <124536228+G3ovani-Andrade@users.noreply.github.com>
2023-10-21 14:56:40 -0300

    mudança do pacote

---
+++ src/main/java/br/senai/labmedicine/dtos/prontuario/ProntuarioResponseDTO.java
@@ -0,0 +1,24 @@
+package br.senai.labmedicine.dtos.prontuario;
+
+import java.util.List;
+
+import br.senai.labmedicine.dtos.consulta.ConsultaResponseDTO;
+import br.senai.labmedicine.dtos.exame.ExameResponseDTO;
+import br.senai.labmedicine.dtos.paciente.PacienteResponseDTO;
+import br.senai.labmedicine.dtos.Dieta.DietaResponseDTO;
+import br.senai.labmedicine.dtos.exercicio.ExercicioResponseDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProntuarioResponseDTO {
+	private PacienteResponseDTO paciente;
+	private List<DietaResponseDTO> dietas;
+	private List<ExameResponseDTO> exames;
+	private List<ExercicioResponseDTO> exercicios;
+	// TODO descomentar abaixo quando consulta estiver disponível
+	private List<ConsultaResponseDTO> consultas;
+}
