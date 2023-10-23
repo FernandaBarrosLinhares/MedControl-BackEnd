a4384238f4832985196624e877ec8496609d0b50
G3ovani-Andrade <124536228+G3ovani-Andrade@users.noreply.github.com>
2023-10-23 15:17:04 -0300

    Correção responsta

--- src/main/java/br/senai/labmedicine/dtos/dieta/DietaCadastroDTO.java
+++ src/main/java/br/senai/labmedicine/dtos/dieta/DietaCadastroDTO.java
@@ -56,7 +56,7 @@ public class DietaCadastroDTO {
             try {
                 this.horario = LocalTime.from(formatoHorario.parse(horario));
             }catch (DateTimeParseException e){
-                throw new DateTimeParseException("Horario inválido", "", 0);
+                throw new DateTimeParseException("Horário inválido", "", 0);
             }
         }
     }
