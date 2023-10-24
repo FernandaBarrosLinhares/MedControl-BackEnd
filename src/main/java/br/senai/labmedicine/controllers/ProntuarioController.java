ad296a1cf6cc1e518c2bb6f988e8f258da35386c
G3ovani-Andrade <124536228+G3ovani-Andrade@users.noreply.github.com>
2023-10-23 21:13:10 -0300

    Remoção classe não usada

--- src/main/java/br/senai/labmedicine/controllers/ProntuarioController.java
+++ src/main/java/br/senai/labmedicine/controllers/ProntuarioController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.senai.labmedicine.dtos.prontuario.ProntuarioRequestDTO;
 import br.senai.labmedicine.dtos.prontuario.ProntuarioResponseDTO;
 import br.senai.labmedicine.services.ProntuarioService;
 
